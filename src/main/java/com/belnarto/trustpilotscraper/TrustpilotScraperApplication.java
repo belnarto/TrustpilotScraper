e9b564036bb539771aa496311257e782bc8cd11c
ZinchenkoAY <AZinchenko@qulix.com>
2022-10-08 07:16:32 +0300

    skeleton

---
+++ src/main/java/com/belnarto/trustpilotscraper/TrustpilotScraperApplication.java
@@ -0,0 +1,13 @@
+package com.belnarto.trustpilotscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustpilotScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrustpilotScraperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/belnarto/trustpilotscraper/TrustpilotScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belnarto.trustpilotscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustpilotScraperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
